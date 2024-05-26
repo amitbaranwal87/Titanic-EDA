9b9d101383db3d1a54e4f09123cce4313cbf2148
amitbaranwal87 <amit.dweil@gmail.com>
2024-05-26 12:14:42 +0530

    Added calculator module
    This module contains 4 basic arithmetic operations

---
+++ mycal.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return(x+y)
+
+def sub(x,y):
+    return(x-y)
+
+def mul(x,y):
+    return(x*y)
+
+def div(x,y):
+    if y == 0:
+        print("division not possible")
+    else: 
+        return(x/y)
